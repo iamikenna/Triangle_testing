37ac2b465e624ae2bb2ab03201d510f3e180fe69
Ikenna <ikennaibezim90@gmail.com>
2020-02-03 23:35:50 -0500

    updated triangle code

--- Triangle.py
+++ Triangle.py
@@ -12,7 +12,7 @@ The primary goal of this file is to demonstrate a simple python program to class
 def classifyTriangle(a,b,c):
     """
     Your correct code goes here...  Fix the faulty logic below until the code passes all of 
-    you test cases. 
+    your test cases. 
     
     This function returns a string with the type of triangle from three integer values
     corresponding to the lengths of the three sides of the Triangle.
@@ -27,15 +27,15 @@ def classifyTriangle(a,b,c):
       BEWARE: there may be a bug or two in this code
     """
 
-    # require that the input values be >= 0 and <= 200
-    if a > 200 or b > 200 or c > 200:
+    # # require that the input values be >= 0 and <= 200
+    if a >= 200 or b >= 200 or c >= 200:
         return 'InvalidInput'
         
-    if a <= 0 or b <= b or c <= 0:
+    if a <= 0 or b <= 0 or c <= 0:
         return 'InvalidInput'
     
-    # verify that all 3 inputs are integers  
-    # Python's "isinstance(object,type) returns True if the object is of the specified type
+    # # verify that all 3 inputs are integers  
+    # # Python's "isinstance(object,type) returns True if the object is of the specified type
     if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
         return 'InvalidInput';
       
@@ -43,15 +43,19 @@ def classifyTriangle(a,b,c):
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
     # of the specified shape is not a triangle
-    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
+    if (a+b) <= c or (a+c) <= b or (b+c) <= a:
         return 'NotATriangle'
-        
+
     # now we know that we have a valid triangle 
-    if a == b and b == a:
-        return 'Equilateral'
-    elif ((a * 2) + (b * 2)) == (c * 2):
-        return 'Right'
-    elif (a != b) and  (b != c) and (a != b):
-        return 'Scalene'
-    else:
-        return 'Isoceles'
+    if a == b and b == c and a == c: #Checking for Equilateral triangles
+        return "Equilateral"
+    elif ( a ** 2 + b ** 2) == c ** 2: #Checking for Right triangles
+        return "Right"
+    elif a != b and a != c and b != c: #Checking for Scalene triangles
+        return "Scalene"
+    elif a == b and a != c and b != c: #Checking for Isoscles triangles
+        return "Isosceles"
+    elif a != b and a == c and b != c: #Checking for Isoscles triangles
+        return "Isosceles"
+    elif a != b and b == c and a != c: #Checking for Isoscles triangles
+        return "Isosceles"
